248e88c86fa3133dc8b98b22cb310404836e8126
Matt Wiley <mattwiley+gitlab@fastmail.com>
2023-07-06 18:04:07 -0400

    Import clean up.

--- be-java/src/main/java/com/wileymab/bookworm/data/yaml/AuthorsYaml.java
+++ be-java/src/main/java/com/wileymab/bookworm/data/yaml/AuthorsYaml.java
@@ -7,11 +7,7 @@ import org.springframework.stereotype.Component;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 @Component
 public class AuthorsYaml {
